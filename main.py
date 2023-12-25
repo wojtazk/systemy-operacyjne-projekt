31b97e9fc277681d81a6d5f0b3b170100f582d8d
Wojciech Kowal <w.kowal202@gmail.com>
2023-12-25 20:11:59 +0100

    implement generating sample data for cpu scheduling

---
+++ main.py
@@ -0,0 +1,6 @@
+# noinspection PyUnresolvedReferences
+from generateSampleData import generate_cpu_scheduling_sample_data
+
+
+# generate test data for CPU scheduling algorithms
+# generate_cpu_scheduling_sample_data()  # FIXME: uncomment to generate new cpu scheduling sample data

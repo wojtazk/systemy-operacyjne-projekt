a966dcd20507dce15ffc521b59ace5a2c1999f43
Wojciech Kowal <w.kowal202@gmail.com>
2023-12-26 19:11:55 +0100

    implement FCFS cpu scheduling simulation

---
+++ cpu_scheduling/FCFS.py
@@ -0,0 +1,26 @@
+from models.Process import Process
+import copy
+
+
+def simulate_fcfs(processes: list[Process]) -> list:
+    """Simulates a FCFS cpu scheduling algorithm"""
+    processes_copy: list[Process] = copy.deepcopy(processes)
+    cpu_clock: int = processes_copy[0].arrival_time
+    simulation_results: list = []
+
+    for process in processes_copy:
+        if cpu_clock < process.arrival_time:
+            cpu_clock = process.arrival_time
+
+        cpu_clock_after_task_execution: int = cpu_clock + process.burst_time
+        process.completion_time = cpu_clock_after_task_execution
+        cpu_clock = cpu_clock_after_task_execution
+
+        process.calculate_turn_around_time()
+        process.calculate_waiting_time()
+
+        # Process.get_properties_as_list()->[arrival_time, burst_time, completion_time, turn_around_time, waiting_time]
+        tmp = process.get_properties_as_list()[2:]  # [completion_time, turn_around_time, waiting_time]
+        simulation_results.append(tmp)
+
+    return simulation_results
--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 from generateSampleData import generate_cpu_scheduling_sample_data
 from loadSampleData import load_cpu_scheduling_sample_data
 
+from cpu_scheduling.FCFS import simulate_fcfs
+
+from helpers import write_data_to_file
+
 
 ############################################################################
 # config
-NUM_OF_SAMPLES = 10_000
-MAX_ARRIVAL_TIME = NUM_OF_SAMPLES ** 2
+NUM_OF_SAMPLES = 100_000
+MAX_ARRIVAL_TIME = 10_000
 MAX_BURST_TIME = 5_000
 
 SIMULATION_SAMPLE_DATA_DIR = 'simulation_sample_data'
@@ -14,14 +18,20 @@ SIMULATION_RESULTS_DIR = 'simulation_results'
 
 CPU_SAMPLE_FILE = SIMULATION_SAMPLE_DATA_DIR + '/' + 'cpu_scheduling_sample_data.txt'
 
-CPU_FCFS_RESULTS_FILE = 'FCFS_simulation_results.txt'
-CPU_ROUND_ROBIN_RESULTS_FILE = 'Round-Robin_simulation_results.txt'
+CPU_FCFS_RESULTS_FILE = SIMULATION_RESULTS_DIR + '/' + 'FCFS_simulation_results.txt'
+CPU_ROUND_ROBIN_RESULTS_FILE = SIMULATION_RESULTS_DIR + '/' + 'Round-Robin_simulation_results.txt'
 ############################################################################
 
+
+############################################################################
+# CPU scheduling
 
 # generate test data for CPU scheduling algorithms
 # FIXME: uncomment to generate new cpu scheduling sample data
 # generate_cpu_scheduling_sample_data(NUM_OF_SAMPLES, MAX_ARRIVAL_TIME, MAX_BURST_TIME, CPU_SAMPLE_FILE)
 
 cpu_scheduling_processes = load_cpu_scheduling_sample_data(CPU_SAMPLE_FILE)
-print()
+
+fcfs_simulation_data: list = simulate_fcfs(cpu_scheduling_processes)
+write_data_to_file(fcfs_simulation_data, CPU_FCFS_RESULTS_FILE)
+############################################################################

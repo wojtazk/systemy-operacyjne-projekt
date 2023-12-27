88af4e1de8b9db7684148318eb47d5f3b8a00183
Wojciech Kowal <w.kowal202@gmail.com>
2023-12-27 12:36:43 +0100

    implement page replacement sample data generation

--- generateSampleData.py
+++ generateSampleData.py
@@ -6,11 +6,20 @@ def generate_cpu_scheduling_sample_data(num_of_samples: int, max_arrival_time: i
                                         output_file: str) -> None:
     sample_data = []
     for i in range(num_of_samples):
-        arrival_time = int((uuid4().int % max_arrival_time) + 1)  # arrival_time in [1, num_of_samples]
-        burst_time = int((uuid4().int % max_burst_time) + 1)  # burst_time in [1, max_burst_time]
+        arrival_time = (uuid4().int % max_arrival_time) + 1  # arrival_time in [1, num_of_samples]
+        burst_time = (uuid4().int % max_burst_time) + 1  # burst_time in [1, max_burst_time]
         sample_data.append([arrival_time, burst_time])
 
     # sort data based on arrival time
     sample_data.sort(key=lambda x: x[0])
+
+    write_data_to_file(sample_data, output_file)
+
+
+def generate_page_replacement_sample_data(num_of_samples: int, max_page_reference_num: int, output_file: str) -> None:
+    sample_data = []
+    for i in range(num_of_samples):
+        page_reference_num = uuid4().int % max_page_reference_num
+        sample_data.append([page_reference_num])
 
     write_data_to_file(sample_data, output_file)
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # noinspection PyUnresolvedReferences
 from generateSampleData import generate_cpu_scheduling_sample_data
+# noinspection PyUnresolvedReferences
+from generateSampleData import generate_page_replacement_sample_data
+
 from loadSampleData import load_cpu_scheduling_sample_data
 
 from cpu_scheduling.FCFS import simulate_fcfs
 from cpu_scheduling.Round_Robin import simulate_round_robin
+
 
 from helpers import write_data_to_file
 
@@ -12,18 +16,23 @@ from helpers import write_data_to_file
 # config
 NUM_OF_SAMPLES = 100_000
 
+# cpu scheduling
 MAX_ARRIVAL_TIME = 10_000
 MAX_BURST_TIME = 5_000
-
 # CPU_TIME_SLICE = 10
 # CPU_TIME_SLICE = 100
 CPU_TIME_SLICE = 1000
 # CPU_TIME_SLICE = 4000  # basically degrade Round-Robin to FCFS
 
+# page replacement
+NUM_OF_PAGE_FRAMES = 1000  # number of pages that can fit into the memory
+MAX_PAGE_REFERENCE_NUM = 10_000
+
 SIMULATION_SAMPLE_DATA_DIR = 'simulation_sample_data'
 SIMULATION_RESULTS_DIR = 'simulation_results'
 
 CPU_SAMPLE_FILE = SIMULATION_SAMPLE_DATA_DIR + '/' + 'cpu_scheduling_sample_data.txt'
+PAGE_SAMPLE_FILE = SIMULATION_SAMPLE_DATA_DIR + '/' + 'page_replacement_sample_data.txt'
 
 CPU_FCFS_RESULTS_FILE = SIMULATION_RESULTS_DIR + '/' + 'FCFS_simulation_results.txt'
 CPU_ROUND_ROBIN_RESULTS_FILE = (
@@ -36,6 +45,7 @@ CPU_ROUND_ROBIN_RESULTS_FILE = (
 # CPU scheduling
 
 # generate test data for CPU scheduling algorithms
+# FIXME: uncomment to generate new cpu sample data
 # generate_cpu_scheduling_sample_data(NUM_OF_SAMPLES, MAX_ARRIVAL_TIME, MAX_BURST_TIME, CPU_SAMPLE_FILE)
 
 # load processes data from file
@@ -48,4 +58,13 @@ write_data_to_file(fcfs_simulation_data, CPU_FCFS_RESULTS_FILE)
 # simulate Round-Robin and write results to file
 round_robin_simulation_data: list = simulate_round_robin(cpu_scheduling_processes, CPU_TIME_SLICE)
 write_data_to_file(round_robin_simulation_data, CPU_ROUND_ROBIN_RESULTS_FILE)
+############################################################################
+
+
+############################################################################
+# page replacement
+
+# generate page replacement sample data
+# FIXME: uncomment to generate new page replacement sample data
+# generate_page_replacement_sample_data(NUM_OF_SAMPLES, MAX_PAGE_REFERENCE_NUM, PAGE_SAMPLE_FILE)
 ############################################################################

6a353df261b16bc4d2da5690dca6b5153340bb4e
Wojciech Kowal <w.kowal202@gmail.com>
2023-12-31 23:28:06 +0100

    correct implementation of Round-Robin

--- cpu_scheduling/Round_Robin.py
+++ cpu_scheduling/Round_Robin.py
@@ -4,52 +4,103 @@ import copy
 
 def simulate_round_robin(processes: list[Process], time_slice: int) -> list:
     """Simulates a Round-Robin cpu scheduling algorithm"""
+    # processes should be sorted by arrival time
+
+    # make local copy of processes array
     processes_copy: list[Process] = copy.deepcopy(processes)
-    num_of_processes_in_queue: int = 0
+    # set cpu clock to arrival time of first process
     cpu_clock: int = processes_copy[0].arrival_time
+    # declare queue array (it queues indexes of processes currently being executed)
+    queue: list[int] = [0]
+    # initialize variable for storing number of processes in queue
+    next_arriving_process_index: int = 1
+    # initialize array for storing simulation results
     simulation_results: list = []
 
-    while True:
-        done: bool = True
+    def _add_arriving_processes_to_queue() -> None:
+        """Adds processes to queue if their arrival time is less than cpu clock"""
+        nonlocal next_arriving_process_index, processes_copy, cpu_clock, queue
 
-        # add arriving processes to the process_queue
-        for i in range(num_of_processes_in_queue, len(processes_copy)):
-            process = processes_copy[i]
-            if process.arrival_time <= cpu_clock:
-                num_of_processes_in_queue += 1
+        for i in range(next_arriving_process_index, len(processes_copy)):
+            p = processes_copy[i]
+            if p.arrival_time <= cpu_clock:
+                # append pages' index to the queue
+                queue.append(i)
+                # increase the next arriving process index
+                next_arriving_process_index += 1
             else:
                 break
 
-        for i in range(num_of_processes_in_queue):
-            process = processes_copy[i]
+    # execute for as long as any task has remaining burst time
+    while True:
+        # initialize done to True
+        # if done is True at the end of the loop, then the simulation is done
+        done: bool = True
 
-            # continue if process is already finished
-            if process.remaining_burst_time == 0:
-                continue
+        # go through processes currently in queue and give each slice of cpu time
+        while len(queue) > 0:
+            # get first process index from the queue
+            current_process_idx = queue[0]
+            # get currently executing current_process
+            current_process = processes_copy[current_process_idx]
 
-            # if tasks with remaining burst time still exist -> the algorithm is not done
+            # if processes still exist in queue -> the algorithm is not done
+            # set done to False
             done = False
 
-            # give process a slice of cpu time
-            # remaining burst time is more than time_slice
-            if process.remaining_burst_time > time_slice:
+            # give current_process a slice of cpu time
+            # if remaining burst time is more than time slice
+            if current_process.remaining_burst_time > time_slice:
+                # add time slice to cpu clock
                 cpu_clock += time_slice
-                process.remaining_burst_time -= time_slice
+                # subtract time slice from processes' remaining burst time
+                current_process.remaining_burst_time -= time_slice
+                # add processes that arrived during execution of the current current_process to the queue
+                _add_arriving_processes_to_queue()
+                # pop current process' index from the queue
+                queue.pop(0)
+                # add current process' index at the end of the queue (schedule it for another execution)
+                queue.append(current_process_idx)
+                # go to the next current_process
                 continue
 
-            # remaining burst time is less than time_slice
-            cpu_clock += process.remaining_burst_time
-            process.remaining_burst_time = 0
-            process.completion_time = cpu_clock
+            # if remaining burst time is less than time_slice
+            # add remaining burst time to cpu clock
+            cpu_clock += current_process.remaining_burst_time
+            # set processes' remaining burst time to 0
+            current_process.remaining_burst_time = 0
+            # set processes' completion time to current cpu clock
+            current_process.completion_time = cpu_clock
+            # pop the processes' index from queue
+            queue.pop(0)
 
-            process.calculate_turn_around_time()
-            process.calculate_waiting_time()
+            # calculate processes' turn around time
+            current_process.calculate_turn_around_time()
+            # calculate processes' waiting time
+            current_process.calculate_waiting_time()
 
-            tmp = process.get_properties_as_list()
+            # get processes' properties as list
+            tmp = current_process.get_properties_as_list()
+            # remove remaining burst time from the list (it would be always 0)
             tmp = tmp[:2] + tmp[3:]  # [arrival_time, burst_time, completion_time, turn_around_time, waiting_time]
+            # append properties to the simulation results array
             simulation_results.append(tmp)
 
+            # add arriving processes to the process_queue
+            _add_arriving_processes_to_queue()
+
+        # if simulation is done
         if done:
+            # break from the while loop
             break
+
+        # if queue is empty and index of next arriving process is less than length of processes_copy array
+        if len(queue) == 0 and next_arriving_process_index < len(processes_copy):
+            # set cpu_clock to arrival time of the next arriving process
+            cpu_clock = processes_copy[next_arriving_process_index].arrival_time
+            # append the next arriving process to the queue
+            queue.append(next_arriving_process_index)
+            # increment the next arriving process index
+            next_arriving_process_index += 1
 
     return simulation_results
